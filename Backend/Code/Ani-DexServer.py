8e7bcaa3dff23f01b616e1f1f6ccb4505e6f86b3
TheArtbot <119063914+TheArtbot@users.noreply.github.com>
2024-07-25 21:04:33 -0500

    Add backend code
    The rest of the models and animal info will come a bit later

---
+++ Backend/Code/Ani-DexServer.py
@@ -0,0 +1,112 @@
+import numpy as np
+import PIL 
+import tensorflow as tf
+import pathlib
+import os
+import shutil
+import random
+import math
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+
+def LoadModels(mainModel,mainEncoding,subModels,subEncodings,specialModels,specialEncodings):
+    # getting the encoding...
+    try:
+        encodingFile = open("Models\\Encoding\\mainEncode.txt","r")
+        for line in encodingFile:
+            mainEncoding = {index:label for index, label in line.lower.split(',')}
+        encodingFile.close()
+    except Exception as e:
+        raise Exception("[Error in LoadModels:main model encoding]",e)
+    
+    try:
+        encodingFile = open("Models\\Encoding\\subEncode.txt","r")
+        for line in encodingFile:
+            group_name, group_encoding = line.split('[')
+            group_encoding.removesuffix("]")
+            subEncodings[group_name] = {index:label for index, label in group_encoding.lower.split(',')}
+        encodingFile.close()
+    except Exception as e:
+        raise Exception("[Error in LoadModels:sub models encoding]",e)
+    
+    try:
+        encodingFile = open("Models\\Encoding\\specialEncode.txt","r")
+        for line in encodingFile:
+            animal_name, special_encoding = line.split('[')
+            special_encoding.removesuffix("]")
+            specialEncodings[animal_name] = {index:label for index, label in special_encoding.lower.split(',')}
+        encodingFile.close()
+    except Exception as e:
+        raise Exception("[Error in LoadModels:special models encoding]",e)
+    
+    
+    # getting the models
+    try:
+        MainModel_folder = pathlib.Path("C:\\Users\\ArtBot\\Desktop\\Uni Stuff\\Year Three\\COMP 3901\\Backend\\Models\\Main Model").with_suffix('')
+        MAIN_MODEL_PATH = str(list(MainModel_folder.glob("*.tflite"))[0].absolute())
+        interpreter = tf.lite.Interpreter(model_path=MAIN_MODEL_PATH)
+        mainModel = interpreter.get_signature_runner('serving_default')
+    except Exception as e:
+        raise Exception("[Error in LoadModels: main model loading]",e)
+    
+    try:
+        SubModels_folder = pathlib.Path("C:\\Users\\ArtBot\\Desktop\\Uni Stuff\\Year Three\\COMP 3901\\Backend\\Models\\Sub Models").with_suffix('')
+        for dir in list(SubModels_folder.glob("*.tflite")):
+            SUB_MODEL_PATH = str(dir.absolute())
+            interpreter = tf.lite.Interpreter(model_path=SUB_MODEL_PATH)
+            subModels[dir.name.split("_")[0]] = interpreter.get_signature_runner('serving_default')
+    except Exception as e:
+        raise Exception("[Error in LoadModels: sub models loading]",e)
+    
+    try:
+        SpecialModels_folder = pathlib.Path("C:\\Users\\ArtBot\\Desktop\\Uni Stuff\\Year Three\\COMP 3901\\Backend\\Models\\Special Models").with_suffix('')
+        for dir in list(SpecialModels_folder.glob("*.tflite")):
+            SPECIAL_MODEL_PATH = str(dir.absolute())
+            interpreter = tf.lite.Interpreter(model_path=SPECIAL_MODEL_PATH)
+            specialModels[dir.name.split("_")[0]] = interpreter.get_signature_runner('serving_default')
+    except Exception as e:
+        raise Exception("[Error in LoadModels: sub models loading]",e)
+    
+    return mainModel, mainEncoding, subModels, subEncodings, specialModels, specialEncodings
+
+def MakePrediction(img_arr, mainModel, mainEncoding, subModels, subEncodings, specialModels, specialEncodings):
+    main_inputdetails = mainModel.get_input_detials()
+    main_inputdict = { input_detail:img_arr for input_detail in main_inputdetails}
+    
+    main_prediction = tf.nn.softmax(mainModel(**main_inputdict)['outputs'])
+    group_label = mainEncoding[np.argmax(main_prediction)]
+    
+    sub_inputdetails = subModels[group_label].get_input_details()
+    sub_inputdict = { input_detail:img_arr for input_detail in sub_inputdetails}
+    
+    sub_prediction = tf.nn.softmax(subModels[group_label](**sub_inputdict)['outputs'])
+    final_prediction = subEncodings[group_label][np.argmax(sub_prediction)]
+    
+    if final_prediction not in specialEncodings.keys(): return (final_prediction,False,None)
+    
+    special_inputdetails = specialModels[final_prediction].get_input_details()
+    special_inputdict = { input_detail:img_arr for input_detail in special_inputdetails}
+    
+    special_prediction = tf.nn.softmax(specialModels[final_prediction](**special_inputdict)['outputs'])
+    specific_prediction = specialEncodings[final_prediction][special_prediction]
+    
+    return (final_prediction,True,specific_prediction)    
+    
+def LoadAnimalInfo(animal_type,animal_specific,special_types):
+    FILE_PATH = "AnimalInfo\\" + animal_type
+    if animal_type in special_types: FILE_PATH = FILE_PATH + "\\" + animal_specific + ".txt"
+    else: FILE_PATH = FILE_PATH + ".txt"
+    
+    info = {}
+    animal_file = open(FILE_PATH,"r")
+    for line in animal_file:
+        header, content = line.split(":/:")
+        info[header] = content
+    
+    return info
+
+def SetUpConnection():
+    pass
